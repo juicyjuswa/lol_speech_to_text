070225cf9cde758d42dd00f5894f51f03cae2d47
juicyjuswa <braganzajoshua523@gmail.com>
2024-02-08 16:54:36 +0800

    Final Code, Less Comment, How to use

--- league_speech_to_text.py
+++ league_speech_to_text.py
@@ -11,58 +11,33 @@ import speech_recognition as sr
 import numpy as np
 import sounddevice as sd
 import wave
-import keyboard
-import pyautogui as pag
-from time import sleep
-
-import speech_recognition as sr
-import numpy as np
-import sounddevice as sd
-import wave
 
 import keyboard
 import pyautogui as pag
 from time import sleep
 
-#File-Name-To-Save
 FILE_NAME = './lol_speech_to_text.wav'
-
-#Recording-Length-(seconds)
 wave_length = 4
-
-# Sampling-Frequency
 sample_rate = 16_000
 
 while True:
     keyboard.wait('v')
     print("RECORDING ***")
-    # Start-Recording 
     data = sd.rec(int(wave_length * sample_rate), sample_rate, channels=1)
     sd.wait()
-   
-    # Normalize
     data = data / data.max() * np.iinfo(np.int16).max
-   
-    # Float-To-Int
     data = data.astype(np.int16)
    
-    #Save-File
     with wave.open(FILE_NAME, mode='wb') as wb:
-        # Monaural
         wb.setnchannels(1)
-        # 16bit=2byte
         wb.setsampwidth(2)
         wb.setframerate(sample_rate)
-        # Convert-To-Byte-String
         wb.writeframes(data.tobytes())
-    #File-Name-To-Save
     filename = "lol_speech_to_text.wav"
     r = sr.Recognizer()
    
     with sr.AudioFile(filename) as source:
-        # Listen-For-The-Data
         audio_data = r.record(source)
-        # Convert-From-Speech-To-Text)
         text = r.recognize_google(audio_data)
         print(text)
 
@@ -70,3 +45,11 @@ while True:
     sleep(0.01)
     pag.write(text)
     keyboard.press_and_release('enter')
+
+    # note you need internet for this to work
+    #STEPS TO USE
+    # 1.)OPEN LEAGUE OF LEGENDS AND FIND A MATCH
+    # 2.)WHILE INSDE THE MATCH, PRESS V TO INITIATE "keyboard.wait('v')" AND RUN THE CODE
+    # 3.) SAY ANY COMMS THROUGH YOUR MIC THAT YOU WOULD LIKE YOUR TEAMMATES TO KNOW
+    # 4.) AFTER TALKING, PRESS ENTER TWICE, CONSECUTIVELY
+    # 5.) WHAT YOU TOLD THE MIC WOULD BE CONVERTED TO TEXT AND AUTOMATICALLY BE TYPED THROUGH IN GAME CHAT WHILE PLAYING
